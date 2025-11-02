93c3d662853e8b82efff4cde0543a14ba0eb143e
Rostislav Litovkin <rostislavlitovkin@gmail.com>
2025-11-02 18:08:47 +0100

    Nova import and export pages

--- PolkadotRoots/Pages/SettingsPageViewModel.cs
+++ PolkadotRoots/Pages/SettingsPageViewModel.cs
@@ -3,6 +3,7 @@ using CommunityToolkit.Mvvm.Input;
 using PlutoFramework.Components.Account;
 using PlutoFramework.Components.Credits;
 using PlutoFramework.Components.Mnemonics;
+using PlutoFramework.Components.Nova;
 using PlutoFramework.Components.Password;
 using PlutoFramework.Components.Settings;
 using PlutoFramework.Model;
@@ -44,6 +45,12 @@ namespace PolkadotRoots.Pages
 
         [RelayCommand]
         public Task DeveloperSettingsAsync() => Shell.Current.Navigation.PushAsync(new DeveloperSettingsPage());
+
+        [RelayCommand]
+        public Task ImportFromNovaAsync() => Shell.Current.Navigation.PushAsync(new NovaExportGuidePage());
+
+        [RelayCommand]
+        public Task ExportToNovaAsync() => Browser.Default.OpenAsync("https://docs.novawallet.io/nova-wallet-wiki/wallet-management/import-an-existing-wallet/import-via-passphrase", BrowserLaunchMode.SystemPreferred);
 
         [RelayCommand]
         public async Task ShowMnemonicsAsync()
